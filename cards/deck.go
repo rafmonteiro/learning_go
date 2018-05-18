f9f07cd6bbd81e96cdda32d8f5bf9013df53476b
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2018-05-17 21:45:52 -0700

    workaround the GO fixed randomization seed

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of `deck` which is
@@ -59,8 +60,11 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		// swap the elements
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
