f148330d6727e3cb1fc872efc914bf44e31d2aa0
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2018-05-20 10:44:48 -0700

    test save to file and new deck from file

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -17,4 +18,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Diamonds" {
 		t.Errorf("Expected first card of Four of Diamonds, but got %v", d[0])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Excepted 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
