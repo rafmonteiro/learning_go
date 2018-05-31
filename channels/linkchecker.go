5b65fab23a845a9fe90910b6b7f5960793d393d0
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2018-05-30 22:25:19 -0700

    1st try to implement channel

--- channels/linkchecker.go
+++ channels/linkchecker.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 )
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		// send msg into the channel
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "It's UP"
 }
 
 func main() {
@@ -22,7 +25,12 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		// creates go routines
+		go checkLink(link, c)
 	}
+	// receives msg from the channel
+	fmt.Println(<-c)
 }
