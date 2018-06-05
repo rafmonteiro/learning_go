d0c5a891f297e3610b1c2a621139e09009795dd6
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2018-06-04 22:28:34 -0700

    passing composite data structures into templates: struct

--- web/templates/main.go
+++ web/templates/main.go
@@ -8,20 +8,22 @@ import (
 
 var tpl *template.Template
 
+type sage struct {
+	Name  string
+	Motto string
+}
+
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main() {
-	sages := map[string]string{
-		"India":    "Gandhi",
-		"USA":      "MLK",
-		"Medidate": "Buddha",
-		"Love":     "Jesus",
-		"Prophet":  "Morpheus",
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
 	}
 
-	err := tpl.Execute(os.Stdout, sages)
+	err := tpl.Execute(os.Stdout, buddha)
 	if err != nil {
 		log.Fatalln(err)
 	}
