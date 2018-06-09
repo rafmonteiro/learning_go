058f2b010ee1e8cfb57900ba1d2bf14261e11ed0
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2018-06-09 12:08:41 -0700

    passing functions to a template

--- web/templates/main.go
+++ web/templates/main.go
@@ -4,26 +4,39 @@ import (
 	"html/template"
 	"log"
 	"os"
+	"strings"
 )
 
 var tpl *template.Template
+
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
+
+func init() {
+	//tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+func firstThree(s string) string {
+	s = strings.TrimSpace(s)
+	s = s[:3]
+	return s
+}
 
 type sage struct {
 	Name  string
 	Motto string
 }
 
-func init() {
-	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
-}
-
 func main() {
-	buddha := sage{
-		Name:  "Buddha",
-		Motto: "The belief of no beliefs",
+
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
 	}
 
-	err := tpl.Execute(os.Stdout, buddha)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", g)
 	if err != nil {
 		log.Fatalln(err)
 	}
